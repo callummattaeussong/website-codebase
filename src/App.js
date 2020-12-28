e976d9a1a22f79396f78f914f2db70fe1ee212c5
callummattaeussong <cmsong@asu.edu>
2020-12-28 16:34:26 -0700

    working home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Home from './components/Home';
+import Background from './images/HomeBackground.jpg';
 
 function App() {
   return (
-    <Home />
+    <div class="uk-background-fixed uk-background-center-center uk-height-medium uk-width-large" 
+          style={{backgroundImage: `url(${Background})`, width:'100%', height:'auto', backgroundSize:'100%'}}
+    >
+      <Home />
+    </div>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,13 @@
 import React from "react";
-import Background from '../images/HomeBackground.jpg';
 
 function Home () {
     return (
-      <div display="grid" class="inline uk-cover-container" style={{width:'100vw', height:'100vh'}}>
-        <img style={{maxHeight:'auto', width: '100%'}} src={Background} alt="Home Background" uk-cover />
-        <div class="uk-overlay uk-light uk-position-bottom" >
-          <h1 className="uk-heading-small">Callum Song</h1>
-          <hr className="uk-divider-icon"/>
-          <h2 className="uk-heading-xsmall" >Computer Science</h2>
-        </div>
+      <div class="uk-container" style={{width:'100vw', height:'100vh', paddingTop:'5vw', color:'white'}}>
+          <div style={{bottom: '2vw'}}>
+            <h1 style={{color:'white'}} className="uk-heading-small">Callum Song</h1>
+            <hr className="uk-divider-icon"/>
+            <h2 style={{color:'white'}} className="uk-heading-xsmall" >Computer Science</h2>
+          </div>
       </div>
     );
 };
