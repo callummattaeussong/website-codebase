fcf3c67605be4c4a31b4777cc72160d77491393d
callummattaeussong <cmsong@asu.edu>
2020-12-29 20:36:46 -0700

    Finally

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -6,7 +6,7 @@ function Repository() {
       id="repository"
       className="uk-card uk-card-default"
       style={{
-        marginTop:'30px',
+        marginTop: "30px",
         paddingTop: "20px",
         paddingBottom: "40px",
         paddingLeft: "30px",
@@ -14,12 +14,78 @@ function Repository() {
         textAlign: "right",
       }}
     >
-      <div class="uk-card-header" style={{paddingTop:'7vw'}}>
-        <h3 className="uk-heading-medium" style={{ paddingLeft: "30px", paddingTop:'7vw'}}>
+      <div class="uk-card-header" style={{ paddingTop: "7vw" }}>
+        <h3
+          className="uk-heading-medium"
+          style={{ paddingLeft: "30px", paddingTop: "7vw" }}
+        >
           Repo
         </h3>
       </div>
       <hr className="uk-divider-icon" />
+
+      <div class="uk-container uk-container-large" style={{textAlign:'left'}}>
+        <div
+          className="uk-child-width-1-2@m uk-grid-small uk-grid-match uk-grid uk-grid-stack"
+          uk-grid=""
+        >
+          <div className="item uk-first-column">
+            <div className="uk-card uk-card-hover uk-card-secondary uk-card-body">
+              <h3 className="uk-card-title">Projects</h3>
+              <p>
+                <b>
+                  <i>Project Bunny (C++)</i>
+                </b>
+              </p>
+              <p>
+                Developed program simulating a population of bunnies through
+                basic life processes such as breeding, death, and randomly
+                occurring natural disasters. <br /> <br /> The program uses
+                object orientation to simulate individual bunny’s traits and
+                features and randomly generated numbers to trigger disasters
+                (e.g. zombie plague, population cull)
+              </p>
+              <hr />
+              <p>
+                <b>
+                  <i>Personal Website</i>
+                </b>
+              </p>
+              <p>
+                I spent a week learning React and 2 days to build my very own
+                website based completely off of react, deployed on GitHub.
+              </p>
+              <hr />
+              <p>
+                <b>
+                  <i>Covid Cinema</i>
+                </b>
+              </p>
+              <p>Linux, Windows</p>
+            </div>
+          </div>
+
+          <div className="item uk-grid-margin uk-first-column">
+            <div className="uk-card uk-card-hover uk-card-secondary uk-card-body">
+              <h3 className="uk-card-title">Interesting Algorithms</h3>
+
+              <p>
+                Out of an interest in algorithms, I've dedicated my time to
+                implementing and understanding some of the algorithms I found
+                interesting through class, leetcode and other sources.
+              </p>
+
+              <br /><br />
+              <hr />
+              <h3 className="uk-card-title">My Philosophy On Code</h3>
+              <p>
+                I hold a strong belief that every problem has an elegant and cleanly written solution that can be executed well!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <br />
       <h3
         style={{
