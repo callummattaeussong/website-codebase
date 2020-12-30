0693d0a0e81931b88ee05625cc60b48573c4dc2c
callummattaeussong <cmsong@asu.edu>
2020-12-29 21:00:13 -0700

    contact me header

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,16 @@ import React from "react";
 function Contact() {
   return (
     <div className="uk-container uk-container-small" id="contactme">
+      
       <div className="uk-card uk-card-default uk-card-hover">
         <div className="uk-card-body" >
+        <h3
+          className="uk-heading-medium"
+          style={{ paddingLeft: "30px", paddingTop: "1vw" }}
+        >
+          Contact Me
+        </h3>
+        <hr className="uk-divider-icon" />
           <form action="https://formspree.io/f/maylpozd" method="POST" target="_blank" >
             <label>
               Your email:
